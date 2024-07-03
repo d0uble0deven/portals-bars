261864ce6871966411bb8d1f7e518133f984ecdd
Dev Govindji <dev.govindji.software@gmail.com>
2024-07-03 14:33:30 -0500

    removes big files, reformats cards in UI

--- src/App.js
+++ src/App.js
@@ -15,24 +15,14 @@ export const App = () => (
     <Canvas camera={{ fov: 75, position: [0, 0, 20] }} eventSource={document.getElementById('root')} eventPrefix="client">
       <color attach="background" args={['#f0f0f0']} />
 
-      <Frame id="01" name={`Blue\nLacy`} author="Lladro" bg="#e4cdac" position={[-1.15, 2, 0]} rotation={[0, 0.5, 0]}>
+      <Frame id="01" name={`Blue\nLacy`} author="Blue Lacy" bg="#e4cdac" position={[-1.65, 2, 0]} rotation={[0, 0.5, 0]}>
         {/* Blue Lacy */}
         {/* https://www.zappyhour.co/happy-hour-details?recordId=recn53pSPpCoZTZ1m */}
-        {/* <GltfPortal scale={2} position={[0, -2.7, -10]} /> */}
         <Environment background files="./BlueLacy.hdr" />
         <pointLight position={[2, 2, -180]} intensity={1} color="#fff" />
-        {/* <Gltf src="./burger.gltf" scale={8} position={[0, 0, 0]} /> */}
-        {/* <Gltf src="./ham_bone.gltf" scale={8} position={[0, 0, 0]} /> */}
-        {/* <Gltf src="./hot_dog.gltf" scale={8} position={[0, 0, 0]} /> */}
         <Gltf src="./beer_bottle.gltf" scale={8} position={[5, -2, 0]} />
-        {/* <Gltf src="./pizza.gltf" scale={8} position={[0, 0, 0]} /> */}
-        {/* <Gltf src="./sub_sandwich.gltf" scale={8} position={[0, 0, 0]} /> */}
-        {/* <Gltf src="./taco.gltf" scale={8} position={[0, 0, 0]} /> */}
         <Gltf src="./wine_bottle.gltf" scale={8} position={[0, -2, -5]} />
         <Gltf src="./cocktail.gltf" scale={8} position={[-5, -2, 0]} />
-
-        {/* <Gltf src="./hamburger.glb" scale={8} position={[0, 0, -150]} /> */}
-        {/* <Gltf src="./wine.glb" scale={8} position={[10, 0, -50]} /> */}
         <pointLight position={[-2, -2, -120]} intensity={1} color="#fff" />
       </Frame>
 
@@ -41,60 +31,30 @@ export const App = () => (
         {/* https://www.zappyhour.co/happy-hour-details?recordId=rec1kkOwy5hUam6hd */}
         <Environment background files="./LuckyDuck.hdr" />
         <pointLight position={[2, 2, -180]} intensity={1} color="#fff" />
-        {/* <Gltf src="./burger.gltf" scale={8} position={[0, 0, 0]} /> */}
-        {/* <Gltf src="./hot_dog.gltf" scale={8} position={[0, 0, 0]} /> */}
         <Gltf src="./beer_bottle.gltf" scale={8} position={[5, -2, 0]} />
-        {/* <Gltf src="./pizza.gltf" scale={8} position={[0, 0, 0]} /> */}
-        {/* <Gltf src="./sub_sandwich.gltf" scale={8} position={[0, 0, 0]} /> */}
-        {/* <Gltf src="./taco.gltf" scale={8} position={[0, 0, 0]} /> */}
         <Gltf src="./ham_bone.gltf" scale={4} position={[0, -2, -5]} />
-        {/* <Gltf src="./wine_bottle.gltf" scale={8} position={[0, -2, -5]} /> */}
         <Gltf src="./cocktail.gltf" scale={8} position={[-5, -2, 0]} />
-
-        {/* <Gltf src="./hamburger.glb" scale={8} position={[0, 0, -150]} /> */}
-        {/* <Gltf src="./wine.glb" scale={8} position={[10, 0, -50]} /> */}
         <pointLight position={[-2, -2, -120]} intensity={1} color="#fff" />
       </Frame>
 
-      <Frame id="03" name={`ATX\nCocina`} author="ATX Cocina" bg="#d1d1ca" position={[1.15, 2, 0]} rotation={[0, -0.5, 0]}>
+      <Frame id="03" name={`ATX\nCocina`} author="ATX Cocina" bg="#d1d1ca" position={[1.65, 2, 0]} rotation={[0, -0.5, 0]}>
         {/* ATX Cocina */}
         {/* https://www.zappyhour.co/happy-hour-details?recordId=recD1rSZ9ooLuLJCh */}
 
-        {/* <Gltf src="Engagement_9mm.glb" scale={0.15} position={[0, -2, 0]} /> */}
         <Environment background files="./ATXCocina.hdr" />
         <pointLight position={[2, 2, -180]} intensity={1} color="#fff" />
-        {/* <Gltf src="./burger.gltf" scale={8} position={[0, 0, 0]} /> */}
-        {/* <Gltf src="./hot_dog.gltf" scale={8} position={[0, 0, 0]} /> */}
-        {/* <Gltf src="./beer_bottle.gltf" scale={8} position={[5, -2, 0]} /> */}
-        {/* <Gltf src="./pizza.gltf" scale={8} position={[0, 0, 0]} /> */}
-        {/* <Gltf src="./sub_sandwich.gltf" scale={8} position={[0, 0, 0]} /> */}
-        {/* <Gltf src="./taco.gltf" scale={8} position={[0, 0, 0]} /> */}
         <Gltf src="./ham_bone.gltf" scale={4} position={[0, -2, -5]} />
-        {/* <Gltf src="./wine_bottle.gltf" scale={8} position={[0, -2, -5]} /> */}
         <Gltf src="./cocktail.gltf" scale={8} position={[-5, -2, 0]} />
-
-        {/* <Gltf src="./hamburger.glb" scale={8} position={[0, 0, -150]} /> */}
-        {/* <Gltf src="./wine.glb" scale={8} position={[10, 0, -50]} /> */}
         <pointLight position={[-2, -2, -120]} intensity={1} color="#fff" />
       </Frame>
 
-      <Frame id="04" name={`Bar\nMoxy`} author="Bar Moxy" bg="#e4cdac" position={[-1.15, 0, 0]} rotation={[0, 0.5, 0]}>
+      <Frame id="04" name={`Bar\nMoxy`} author="Bar Moxy" bg="#e4cdac" position={[-1.65, 0, 0]} rotation={[0, 0.5, 0]}>
         {/* Bar Moxy */}
         {/* https://www.zappyhour.co/happy-hour-details?recordId=recMSQ6HwVsTPa8Ci */}
         <Environment background files="./BarMoxy.hdr" />
         <pointLight position={[2, 2, -180]} intensity={1} color="#fff" />
-        {/* <Gltf src="./burger.gltf" scale={8} position={[0, 0, 0]} /> */}
-        {/* <Gltf src="./hot_dog.gltf" scale={8} position={[0, 0, 0]} /> */}
         <Gltf src="./beer_bottle.gltf" scale={8} position={[5, -2, 0]} />
-        {/* <Gltf src="./pizza.gltf" scale={8} position={[0, 0, 0]} /> */}
-        {/* <Gltf src="./sub_sandwich.gltf" scale={8} position={[0, 0, 0]} /> */}
-        {/* <Gltf src="./taco.gltf" scale={8} position={[0, 0, 0]} /> */}
-        {/* <Gltf src="./ham_bone.gltf" scale={4} position={[0, -2, -5]} /> */}
-        {/* <Gltf src="./wine_bottle.gltf" scale={8} position={[0, -2, -5]} /> */}
         <Gltf src="./cocktail.gltf" scale={8} position={[0, -2, -5]} />
-
-        {/* <Gltf src="./hamburger.glb" scale={8} position={[0, 0, -150]} /> */}
-        {/* <Gltf src="./wine.glb" scale={8} position={[10, 0, -50]} /> */}
         <pointLight position={[-2, -2, -120]} intensity={1} color="#fff" />
       </Frame>
 
@@ -103,113 +63,41 @@ export const App = () => (
         {/* https://www.zappyhour.co/happy-hour-details?recordId=recjg3w9agW47fJDB */}
         <Environment background files="./Hanks.hdr" />
         <pointLight position={[2, 2, -180]} intensity={1} color="#fff" />
-        {/* <Gltf src="./burger.gltf" scale={8} position={[0, 0, 0]} /> */}
-        {/* <Gltf src="./hot_dog.gltf" scale={8} position={[0, 0, 0]} /> */}
         <Gltf src="./beer_bottle.gltf" scale={8} position={[5, -2, 5]} />
         <Gltf src="./ham_bone.gltf" scale={4} position={[5, -2, -5]} />
         <Gltf src="./wine_bottle.gltf" scale={8} position={[-5, -2, -5]} />
         <Gltf src="./cocktail.gltf" scale={8} position={[-5, -2, 5]} />
-        {/* <Gltf src="./pizza.gltf" scale={8} position={[0, 0, 0]} /> */}
-        {/* <Gltf src="./sub_sandwich.gltf" scale={8} position={[0, 0, 0]} /> */}
-        {/* <Gltf src="./taco.gltf" scale={8} position={[0, 0, 0]} /> */}
-
-        {/* <Gltf src="./hamburger.glb" scale={8} position={[0, 0, -150]} /> */}
-        {/* <Gltf src="./wine.glb" scale={8} position={[10, 0, -50]} /> */}
         <pointLight position={[-2, -2, -120]} intensity={1} color="#fff" />
-        {/* <Html
-                position={ [ 0, 0, 0] }
-                wrapperClass="label"
-                center
-                distanceFactor={ 40 }
-                // occlude={ [ sphere, cube ] }
-                // onOcclude={(hidden) => null}
-              >
-                <img
-  class="fit-picture"
-  src="legacy.png"
-  alt="Grapefruit slice atop a pile of other slices" />
-                <div class="card">
-                Govindji’s mission statement
-
-                  <ul>
-                    <span class="cardTitle">
-                    18K Yellow Gold Diamond Ring 
-                    </span>
-                    <li>
-                    DR1146 
-                    </li>
-                    <li>
-                    Handcrafted 18K White Gold Diamond Band
-                    </li>
-                    <li>
-                    Diamond Weight: 0.45ct
-                    </li>
-                    <li>
-                    $1,807
-                    </li>
-                  </ul>
-
-                </div>
-                            
-              </Html> */}
       </Frame>
 
-      <Frame id="06" name="Saturn" author="Omar Faruq Tawsif" bg="#d1d1ca" position={[1.15, 0, 0]} rotation={[0, -0.5, 0]}>
+      <Frame id="06" name="Saturn" author="Omar Faruq Tawsif" bg="#d1d1ca" position={[1.65, 0, 0]} rotation={[0, -0.5, 0]}>
         {/* Saturn */}
         {/* https://www.zappyhour.co/happy-hour-details?recordId=recj62oKRRYxT972C */}
         <Environment background files="./Saturn.hdr" />
         <pointLight position={[2, 2, -180]} intensity={1} color="#fff" />
-        {/* <Gltf src="./burger.gltf" scale={8} position={[0, 0, 0]} /> */}
         <Gltf src="./ham_bone.gltf" scale={4} position={[-5, -2, 0]} />
-        {/* <Gltf src="./hot_dog.gltf" scale={8} position={[0, 0, 0]} /> */}
         <Gltf src="./beer_bottle.gltf" scale={8} position={[5, -2, 0]} />
-        {/* <Gltf src="./pizza.gltf" scale={8} position={[0, 0, 0]} /> */}
-        {/* <Gltf src="./sub_sandwich.gltf" scale={8} position={[0, 0, 0]} /> */}
-        {/* <Gltf src="./taco.gltf" scale={8} position={[0, 0, 0]} /> */}
-        {/* <Gltf src="./wine_bottle.gltf" scale={8} position={[0, -2, -5]} /> */}
         <Gltf src="./cocktail.gltf" scale={8} position={[0, -2, -5]} />
         <pointLight position={[-2, -2, -120]} intensity={1} color="#fff" />{' '}
       </Frame>
 
-      <Frame id="07" name={`ex1`} author="Omar Faruq Tawsif" bg="#e4cdac" position={[-1.15, -2, 0]} rotation={[0, 0.5, 0]}>
+      <Frame id="07" name={`ex1`} author="Omar Faruq Tawsif" bg="#e4cdac" position={[-1.65, -2, 0]} rotation={[0, 0.5, 0]}>
         <Gltf src="pickles_3d_version_of_hyuna_lees_illustration-transformed.glb" scale={8} position={[0, -0.7, -2]} />
       </Frame>
       <Frame id="08" name="ex2" author="Omar Faruq Tawsif" position={[0, -2, 0]}>
         <Gltf src="fiesta_tea-transformed.glb" position={[0, -2, -3]} />
       </Frame>
-      <Frame id="09" name="ex3" author="Omar Faruq Tawsif" bg="#d1d1ca" position={[1.15, -2, 0]} rotation={[0, -0.5, 0]}>
+      <Frame id="09" name="ex3" author="Omar Faruq Tawsif" bg="#d1d1ca" position={[1.65, -2, 0]} rotation={[0, -0.5, 0]}>
         <Gltf src="still_life_based_on_heathers_artwork-transformed.glb" scale={2} position={[0, -0.8, -4]} />
       </Frame>
-
-      {/* <Frame id="08" name="diner" author="Waffle House" position={[-1.15, -2, 0]} rotation={[0, 0.5, 0]}>
-        <Gltf src="Diner.glb" scale={0.25} position={[20, -2, 0]} />
-        <pointLight position={[0, 10, -5]} intensity={1} color="#fff" />
-      </Frame> */}
-      {/* <Frame id="09" name="cyber" author="Elon Musk" bg="#d1d1ca" position={[1.15, -2, 0]} rotation={[0, -0.5, 0]}>
-        <Gltf src="cybertruck/cybertruck_wrap.gltf" scale={1} position={[-3, -2, -5]} />
-        <pointLight position={[0, 10, -5]} intensity={1} color="#fff" />
-      </Frame> */}
-
       <Rig />
-      <Preload all />
+      <Preload camera />
     </Canvas>
   </>
 )
 
-// function GltfPortal({ scale, position }) {
-//   const currentUrl = window.location.href
-
-//   return (
-//     <>
-//       <pointLight position={[0, 1, -19]} intensity={1} color="#fff" />
-//       <Gltf src="cow/COW.gltf" scale={scale} position={position} />
-//       <pointLight position={[0, 5, 5]} intensity={1} color="#fff" />
-//       {/* <Gltf src="Engagement_9mm.glb" scale={scale} position={position} /> */}
-//     </>
-//   )
-// }
-
-function Frame({ id, name, author, bg, width = 1, height = 1.61803398875, children, ...props }) {
+// function Frame({ id, name, author, bg, width = 1, height = 1.61803398875, children, ...props }) {
+function Frame({ id, name, author, bg, width = 1.61803398875, height = 1, children, ...props }) {
   const portal = useRef()
   const [, setLocation] = useLocation()
   const [, params] = useRoute('/item/:id')
@@ -218,13 +106,15 @@ function Frame({ id, name, author, bg, width = 1, height = 1.61803398875, childr
   useFrame((state, dt) => easing.damp(portal.current, 'blend', params?.id === id ? 1 : 0, 0.2, dt))
   return (
     <group {...props}>
-      <Text font={suspend(medium).default} fontSize={0.2} anchorY="top" anchorX="left" lineHeight={0.8} position={[-0.375, 0.715, 0.01]} material-toneMapped={false}>
+      <Text font={suspend(medium).default} fontSize={0.2} anchorY="top" anchorX="left" lineHeight={0.8} position={[-0.375, 0.415, 0.01]} material-toneMapped={false}>
+        {/* <Text font={suspend(medium).default} fontSize={0.2} anchorY="top" anchorX="left" lineHeight={0.8} position={[-0.375, 0.715, 0.01]} material-toneMapped={false}> */}
         {name}
       </Text>
-      <Text font={suspend(regular).default} fontSize={0.1} anchorX="right" position={[0.4, -0.659, 0.01]} material-toneMapped={false}>
-        /{id}
+      <Text font={suspend(regular).default} fontSize={0.1} anchorX="right" position={[0.7, -0.359, 0.01]} material-toneMapped={false}>
+        {/* <Text font={suspend(regular).default} fontSize={0.1} anchorX="right" position={[0.4, -0.659, 0.01]} material-toneMapped={false}> */}/{id}
       </Text>
-      <Text font={suspend(regular).default} fontSize={0.04} anchorX="right" position={[0.0, -0.677, 0.01]} material-toneMapped={false}>
+      <Text font={suspend(regular).default} fontSize={0.04} anchorX="right" position={[0.0, -0.377, 0.01]} material-toneMapped={false}>
+        {/* <Text font={suspend(regular).default} fontSize={0.04} anchorX="right" position={[0.0, -0.677, 0.01]} material-toneMapped={false}> */}
         {author}
       </Text>
       <mesh name={id} onDoubleClick={(e) => (e.stopPropagation(), setLocation('/item/' + e.object.name))} onPointerOver={(e) => hover(true)} onPointerOut={() => hover(false)}>
